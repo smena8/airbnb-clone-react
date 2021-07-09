d285671434c4a5c27598fd7b000f84f329dddacc
smena8 <puttinonthefritz@protonmail.com>
2021-07-08 22:46:05 -0700

    heroku app and json changes

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-const express = require('express')
+const express = require('express');
+const { resolve } = require('path');
 const path = require('path');
 
 const app = express()
 const port = process.env.PORT || 5000 // Heroku will need the PORT environment variable
 
-app.use(express.static(path.join(__dirname, 'build')));
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'build')));
+    app.get('*', (req, res) => {
+        req.sendfile(path, resolve(__dirname, 'build', 'index.html'));
+    })
+}
 
-app.listen(port, () => console.log(`App is live on port ${port}!`))
+app.listen(port, (err) => {
+    if (err) {
+        return console.log(err);
+    } else {
+    console.log(`App is live on port ${port}!`);
+    }
+});
